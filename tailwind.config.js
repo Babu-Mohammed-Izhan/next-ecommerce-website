790c1db7a7070ee3fe52dff488665b781f41a0da
Izhan <babuizhan1234@gmail.com>
2021-12-17 19:32:01 +0530

    Added backgroud images

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "all-cases":
+          "url(https://res.cloudinary.com/dm8ogh4lv/image/upload/v1639747333/allcases_asaqb4.jpg)",
+        "anime-cases":
+          "url(https://res.cloudinary.com/dm8ogh4lv/image/upload/v1639747335/anime_phone_case_dgxu1p.jpg)",
+        "superhero-cases":
+          "url(https://res.cloudinary.com/dm8ogh4lv/image/upload/v1639747335/marvelcase_j7l7yt.webp)",
+      },
+    },
   },
   variants: {
     extend: {},
